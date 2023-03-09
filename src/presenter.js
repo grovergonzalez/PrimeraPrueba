a973a272f311a91467a8428bfe85aec3bb22c317
grovergonzalez <grover.gonzalez@ucb.edu.bo>
2023-03-09 09:03:14 -0400

    Ingresar Titulo y Mostrarlo en pantalla

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,42 @@
-import sumar from "./sumador";
+document.getElementById('formulario').addEventListener('submit', guardarPost);
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+function guardarPost(e) {
+  let titulo = document.getElementById('titulo').value;
+  
+  let post = {
+    titulo,
+  };
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+  if(localStorage.getItem('Posts') === null) {
+    let post = [];
+    post.push(post);
+    localStorage.setItem('Posts', JSON.stringify(post));
+  } else {
+    let post = JSON.parse(localStorage.getItem('Posts'));
+    post.push(nota);
+    localStorage.setItem('Posts', JSON.stringify(post));
+  }
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  getPosts();
+  document.getElementById('formulario').reset();
+  e.preventDefault();
+}
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-});
+function getPosts() {
+  let post = JSON.parse(localStorage.getItem('Posts'));
+  let postView = document.getElementById('Posts');
+  postView.innerHTML = '';
+  fecha = new Date();
+  for(let i = 0; i < post.length; i++) {
+    let titulo = post[i].titulo;
+
+    postView.innerHTML += `<div class="card mb-3">
+        <div class="card-body">
+        <p>${fecha}</p>
+        <p>${titulo} </p>
+          </p>
+        </div>
+      </div>`;
+  }
+}
+getPosts();
